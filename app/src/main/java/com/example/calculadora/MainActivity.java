7aea24f5a9d3998e232674db731ac6f22ae504ea
Jose Bandera <jbandera@Defensoria.gov.co>
2020-05-23 10:00:57 -0500

    Validacion realizada y mensajes bilingues

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -20,53 +20,93 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         numero_uno = findViewById(R.id.txtNumeroUno);
-        numero_dos=findViewById(R.id.txtNumeroDos);
+        numero_dos = findViewById(R.id.txtNumeroDos);
         resultado = findViewById(R.id.lblResultado);
         mensajes = findViewById(R.id.txtMensajes);
         Operacion = findViewById(R.id.spOperacion);
-        String [] opciones = {"Sumar","Restar","Multiplicar","Dividir"};
-        ArrayAdapter<String> adaptador = new ArrayAdapter<String>( this, android.R.layout.simple_spinner_item, opciones);
+        String[] opciones = {"Sumar", "Restar", "Multiplicar", "Dividir"};
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, opciones);
         Operacion.setAdapter(adaptador);
     }
 
-    public void Calcular (View v) {
-    int num_uno, num_dos, resultados;
-    String Operador1 = numero_uno.getText().toString();
-    String Operador2 = numero_dos.getText().toString();
-    String opcion = Operacion.getSelectedItem().toString();
+    public void Calcular(View v) {
+        int num_uno, num_dos, resultados;
+        //String Operador1 = numero_uno.getText().toString();
+        //String Operador2 = numero_dos.getText().toString();
+        String opcion = Operacion.getSelectedItem().toString();
 
-    if ((Operador1.length() == 0) || (Operador2.length() == 0)) {
+    /*if ((Operador1.length() == 0) || (Operador2.length() == 0)) {
         Toast.makeText(this, "DEBE INGRESAR UN VALOR EN AMBAS CELDAS", Toast.LENGTH_LONG).show();
         //mensajes.setText("DEBE INGRESAR UN VALOR EN AMBAS CELDAS");
     }
-    else {
-        num_uno = Integer.parseInt(numero_uno.getText().toString());
-        num_dos = Integer.parseInt(numero_dos.getText().toString());
-
-
-        if (opcion.equals("Sumar")) {
+    else {*/
+        if (validar()) {
+            num_uno = Integer.parseInt(numero_uno.getText().toString());
+            num_dos = Integer.parseInt(numero_dos.getText().toString());
             resultados = num_uno + num_dos;
-            resultado.setText("" + resultados);
-        } else if (opcion.equals("Restar")) {
-            resultados = num_uno - num_dos;
-            resultado.setText("" + resultados);
-        } else if (opcion.equals("Multiplicar")) {
-            resultados = num_uno * num_dos;
-            resultado.setText("" + resultados);
-        } else if (num_dos != 0) {
-            resultados = num_uno / num_dos;
-            resultado.setText("" + resultados);
-        } else {
-            Toast.makeText(this, "EL DIVISOR DEBE SER DIFERENTE A CERO 0.", Toast.LENGTH_LONG).show();
-            //mensajes.setText("EL DIVISOR DEBE SER DIFERENTE A CERO 0");
+
+            if (opcion.equals("Sumar")) {
+                resultados = num_uno + num_dos;
+                resultado.setText("" + resultados);
+            } else if (opcion.equals("Restar")) {
+                resultados = num_uno - num_dos;
+                resultado.setText("" + resultados);
+            } else if (opcion.equals("Multiplicar")) {
+                resultados = num_uno * num_dos;
+                resultado.setText("" + resultados);
+            } else if (num_dos != 0) {
+                resultados = num_uno / num_dos;
+                resultado.setText("" + resultados);
+            } else {
+                Toast.makeText(this, "EL DIVISOR DEBE SER DIFERENTE A CERO 0.", Toast.LENGTH_LONG).show();
+                //mensajes.setText("EL DIVISOR DEBE SER DIFERENTE A CERO 0");
+            }
+
         }
     }
-    }
-    public void aux_limpiar(View v){
+
+    /*      if (opcion.equals("Sumar")) {
+              resultados = num_uno + num_dos;
+              resultado.setText("" + resultados);
+          } else if (opcion.equals("Restar")) {
+              resultados = num_uno - num_dos;
+              resultado.setText("" + resultados);
+          } else if (opcion.equals("Multiplicar")) {
+              resultados = num_uno * num_dos;
+              resultado.setText("" + resultados);
+          } else if (num_dos != 0) {
+              resultados = num_uno / num_dos;
+              resultado.setText("" + resultados);
+          } else {
+              Toast.makeText(this, "EL DIVISOR DEBE SER DIFERENTE A CERO 0.", Toast.LENGTH_LONG).show();
+              //mensajes.setText("EL DIVISOR DEBE SER DIFERENTE A CERO 0");
+          }
+       //resultado.setText("" + resultados);
+      }
+
+      }*/
+    public void aux_limpiar(View v) {
         numero_uno.setText("");
         numero_dos.setText("");
         resultado.setText("");
         mensajes.setText("");
         numero_uno.requestFocus();
     }
+
+    public boolean validar() {
+        String error_numero_uno, error_numero_dos;
+        error_numero_uno = getResources().getString(R.string.error_numero_uno);
+        error_numero_dos = getResources().getString(R.string.error_numero_dos);
+        if (numero_uno.getText().toString().isEmpty()) {
+            numero_uno.setError(error_numero_uno);
+            numero_uno.requestFocus();
+            return false;
+        } else if (numero_dos.getText().toString().isEmpty()) {
+            numero_dos.setError(error_numero_uno);
+            numero_dos.requestFocus();
+            return false;
+        }
+        return false;
+    }
+
 }
